c394bdba0ec0fa57db0d4027ba60d5267835dcf5
Diana <diana.derkach@fojin.tech>
2022-07-04 18:17:18 +0300

    make main screen ui

--- src/components/Main/components/CategoryCard.js
+++ src/components/Main/components/CategoryCard.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, Text, View, StyleSheet } from "react-native";
 
-export const CategoryCard = () => {
+export const CategoryCard = ({text, color, img}) => {
   return (
-    <View>
-
-    </View>
+    <ImageBackground style={styles.container} source={img} imageStyle={styles.borderRadius}>
+      <Text style={styles.title}>{text}</Text>
+      <View style={[styles.button]}>
+        <Text style={[styles.buttonText, { color: color }]}>Start</Text>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 96,
+    width: 340,
+    paddingHorizontal: 32,
+    marginTop: 20,
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#FFF',
+    width: 58,
+    height: 20,
+  },
+  buttonText: {
+    fontSize: 10,
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 16,
+    color: '#FFF',
+  },
+  borderRadius: {
+    borderRadius: 10,
+  }
+});
--- src/components/Main/main.component.js
+++ src/components/Main/main.component.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { Image, Text, View, StyleSheet, ImageBackground } from "react-native";
-import { CustomHeader } from "./components/CustomHeader";
 import { CategoryCard } from "./components/CategoryCard";
 
 export const MainComponent = () => {
+  const lightBlue = require('../../assets/img/categoryBackgrounds/light-blue.png');
+  const lightViolet = require('../../assets/img/categoryBackgrounds/light-violet.png');
+  const blue = require('../../assets/img/categoryBackgrounds/light-blue.png');
   return (
     <View style={styles.background}>
         <ImageBackground source={require('../../assets/img/headerBackground.png')} imageStyle={styles.borderRadius} style={styles.header} resizeMode={"cover"}>
           <Text style={styles.text}>Choose questions pack to play</Text>
           <Image source={require('../../assets/img/idea.png')} style={styles.image}/>
         </ImageBackground>
-      <CategoryCard text='JS questions' img=''/>
-      <CategoryCard text='React questions' img=''/>
-      <CategoryCard text='Redux questions' img=''/>
+      <View style={styles.categories}>
+        <CategoryCard text='JS questions' color={'#8EA3E8'} img={lightBlue}/>
+        <CategoryCard text='React questions' color={'#A069D7'} img={lightViolet}/>
+        <CategoryCard text='Redux questions' color={'#4F95D0'} img={blue}/>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
+    height: '100%',
     backgroundColor: '#F5F5F5',
   },
   text: {
@@ -39,8 +44,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 184,
   },
+  categories: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   borderRadius: {
-
     borderBottomRightRadius: 40,
     borderBottomLeftRadius: 40,
   }
